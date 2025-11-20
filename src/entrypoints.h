28a7bcbaa57bce8c24c57f2ab1258c4cfa247ac1
Norihiro Kamae <norihiro@nagater.net>
2025-11-20 09:18:43 +0900

    Implement to query menu and widgets, trigger menu

---
+++ src/entrypoints.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void menu_list(obs_data_t *request, obs_data_t *response, void *priv_data);
+void menu_trigger(obs_data_t *request, obs_data_t *response, void *priv_data);
+void widget_list(obs_data_t *request, obs_data_t *response, void *priv_data);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
