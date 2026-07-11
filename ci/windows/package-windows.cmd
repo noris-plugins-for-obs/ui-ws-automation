5bd887085c7e572213c485889c892ce34a0baaca
Norihiro Kamae <norihiro@nagater.net>
2026-07-11 18:53:02 +0900

    .github: Build ARM64 for Windows and OBS 32

--- ci/windows/package-windows.cmd
+++ ci/windows/package-windows.cmd
@@ -8,10 +8,10 @@ set /p PackageVersion=<package-version.txt
 del package-version.txt
 
 REM Package ZIP archive
-7z a "%PluginName%-%PackageVersion%-obs%1-Windows.zip" "..\release\*"
+7z a "%PluginName%-%PackageVersion%-obs%1-Windows-%2.zip" "..\release\*"
 
 REM Build installer
-iscc ..\build\installer-Windows.generated.iss /O. /F"%PluginName%-%PackageVersion%-obs%1-Windows-Installer"
+iscc ..\build\installer-Windows.generated.iss /O. /F"%PluginName%-%PackageVersion%-obs%1-Windows-%2-Installer"
 
-certutil.exe -hashfile "%PluginName%-%PackageVersion%-obs%1-Windows.zip" SHA1
-certutil.exe -hashfile "%PluginName%-%PackageVersion%-obs%1-Windows-Installer.exe" SHA1
+certutil.exe -hashfile "%PluginName%-%PackageVersion%-obs%1-Windows-%2.zip" SHA1
+certutil.exe -hashfile "%PluginName%-%PackageVersion%-obs%1-Windows-%2-Installer.exe" SHA1
